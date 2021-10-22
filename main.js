01c9c6f34c0cdb300bd9b6d3c9124ecc3c36e7d1
irmaktk <73905294+irmaktk@users.noreply.github.com>
2021-10-22 12:04:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const path = "file:///C:/Users/ASUS/OneDrive/Desktop/upload/upload.html";
+const searchBox = document.querySelector(".search-box");
+const navBtnContainer = document.querySelector(".nav-btn-container");
+const searchBtn = document.querySelector(".search-btn");
+const closeBtn = document.querySelector(".close-btn");
+
+const models = document.querySelector("#models");
+const upload = document.querySelector("#upload");
+
+
+upload.addEventListener("click", () =>  {
+  window.location.href = path;
+});
+
+searchBtn.addEventListener("click", () => {
+  searchBox.classList.add("active");
+  navBtnContainer.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  searchBox.classList.remove("active");
+  navBtnContainer.classList.remove("active");
+});
---
+++ upload.js
@@ -0,0 +1,8 @@
+const path = "file:///C:/Users/ASUS/OneDrive/Desktop/upload/main.html";
+
+const main = document.querySelector("#main");
+
+main.addEventListener("click", () => {
+  window.location.href =  "file:///C:/Users/ASUS/OneDrive/Desktop/upload/main.html";  
+  });
+
