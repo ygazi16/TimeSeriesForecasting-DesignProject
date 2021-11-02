3e7bfe579cca16dcd9e13477540500d6fd6a1f6a
ygazi16 <ygazi16@ku.edu.tr>
2021-11-02 23:52:09 +0300

    upload.js 1.3

--- upload.js
+++ upload.js
@@ -1,18 +1,27 @@
 const path = "file:///C:/Users/ASUS/OneDrive/Desktop/upload/main.html";
 var fs = require('fs');
 var parse = require('csv-parse');
+
+
+
 var csvData= document.getElementById("formFileLg");
-fs.createReadStream(req.file.path)
-    .pipe(parse({delimiter: ':'}))
-    .on('data', function(csvrow) {
-        console.log(csvrow);
-        //do something with csvrow
-        csvData.push(csvrow);
-    })
-    .on('end',function() {
-      //do something with csvData
-      console.log(csvData);
-    });
+var button = document.getElementById("clickbutton");
+
+
+button.onclick = function(){
+  fs.createReadStream(req.file.path)
+      .pipe(parse({delimiter: ':'}))
+      .on('data', function(csvrow) {
+          console.log(csvrow);
+          //do something with csvrow
+          csvData.push(csvrow);
+      })
+      .on('end',function() {
+        //do something with csvData
+        console.log(csvData);
+      });
+}
+
 const main = document.querySelector("#main");
 
 main.addEventListener("click", () => {
