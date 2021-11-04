88585382f3a7fbb52d09ec9aec50367e4b9200f4
ygazi16 <ygazi16@ku.edu.tr>
2021-11-04 21:28:05 +0300

    upload.html.1.1

--- upload.js
+++ upload.js
@@ -5,10 +5,10 @@ var parse = require('csv-parse');
 
 
 var csvData= document.getElementById("formFileLg");
-var button = document.getElementById("clickbutton");
+var button1 = document.getElementById("clickbutton1");
+var button2 = document.getElementById("clickbutton2");
 
-
-button.onclick = function(){
+button1.onclick = function(){
   fs.createReadStream(req.file.path)
       .pipe(parse({delimiter: ':'}))
       .on('data', function(csvrow) {
@@ -20,6 +20,33 @@ button.onclick = function(){
         //do something with csvData
         console.print(csvData);
       });
+}
+
+button2.onclick() = function saveTextAsFile()
+{
+    var textToWrite = document.getElementById("inputTextToSave").value;
+    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+    var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
+      var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null)
+    {
+        // Chrome allows the link to be clicked
+        // without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    }
+    else
+    {
+        // Firefox requires the link to be added to the DOM
+        // before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
 }
 
 const main = document.querySelector("#main");
