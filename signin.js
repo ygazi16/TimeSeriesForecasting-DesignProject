246db68baaedab2ab132405980eb273fb480a63d
ygazi16 <ygazi16@ku.edu.tr>
2021-11-12 00:35:17 +0300

    google-auth1.0

--- signin.js
+++ signin.js
@@ -3,6 +3,32 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const repassword = document.getElementById('repassword');
+const express = require('express');
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.');
+
+const app = express();
+
+passport.use(new GoogleStrategy({
+  clientID = keys.google.ClientID,
+  clientSecret = keys.google.ClientSecret,
+  callbackURL: 'auth/google/callback'
+}, (accessToken) => {
+  console.log(accessToken);
+} ));
+
+
+//{"web":{"client_id":"511950486271-tuege6cfons5v671idgt2crhcgijvt52.apps.googleusercontent.com","project_id":"tsf-designproject","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost:5000/auth/google/callback"],"javascript_origins":["http://127.0.0.1:3000"]}}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+
+
+
+
 
 function error(input, message) {
     input.className = 'form-control is-invalid';
