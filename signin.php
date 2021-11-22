2ff78dd66842c2d5c9b27e773f6da7d4c040977a
irmaktk <73905294+irmaktk@users.noreply.github.com>
2021-11-22 22:50:30 +0300

    sign up php

--- signin.php
+++ signin.php
@@ -1,32 +1,73 @@
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <title>Sign In</title>
+	<title>SIGN UP</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
- <div id="frm">
-    <form action="do.php" method="POST">
-        <p >
-            <label >username:</label>
-            <input type="text" id="user" name="user">
+     <form action="signup-check.php" method="post">
+     	<h2>SIGN UP</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
 
-        </p>
+          <?php if (isset($_GET['success'])) { ?>
+               <p class="success"><?php echo $_GET['success']; ?></p>
+          <?php } ?>
 
-        <p >
-            <label >password:</label>
-            <input type="password" id="pass" name="pass">
+          
+          <label>User Name</label>
+          <?php if (isset($_GET['uname'])) { ?>
+               <input type="text" 
+                      name="uname" 
+                      placeholder="User Name"
+                      value="<?php echo $_GET['uname']; ?>"><br>
+          <?php }else{ ?>
+               <input type="text" 
+                      name="uname" 
+                      placeholder="User Name"><br>
+          <?php }?>
 
-        </p>
-        <p>
-            <input type="submit" id ="button" value="submit">
-        </p>
-       
-    </form>
 
- </div>
+     	<label>Password</label>
+     	<input type="password" 
+                 name="password" 
+                 placeholder="Password"><br>
+
+        <label>Email</label>
+          <?php if (isset($_GET['Email'])) { ?>
+               <input type="text" 
+                      name="Email" 
+                      placeholder="Email"
+                      value="<?php echo $_GET['Email']; ?>"><br>
+          <?php }else{ ?>
+               <input type="text" 
+                      name="Email" 
+                      placeholder="Email"><br>
+
+          <?php }?>
+          
+          <label>Phone</label>
+          <?php if (isset($_GET['Phone'])) { ?>
+               <input type="text" 
+                      name="Phone" 
+                      placeholder="Phone"
+                      value="<?php echo $_GET['Phone']; ?>"><br>
+          <?php }else{ ?>
+               <input type="text" 
+                      name="Phone" 
+                      placeholder="Phone"><br>
+          <?php }?>
+
+          <label>Re Password</label>
+          <input type="password" 
+                 name="re_password" 
+                 placeholder="Re_Password"><br>
+
+                 
+
+     	<button type="submit">Sign Up</button>
+          <a href="login.php" class="ca">Already have an account?</a>
+     </form>
 </body>
 </html>
---
+++ signup-check.php
@@ -0,0 +1,86 @@
+<?php 
+session_start(); 
+$host = "localhost";
+$user = "root";
+$password ="dexter";
+$db = "login";
+
+
+$con= mysqli_connect($host, $user, $password);
+mysqli_select_db($con, $db) or die($connect_error);
+
+if (isset($_POST['uname']) && isset($_POST['password'])
+    && isset($_POST['Email']) && isset($_POST['Phone']) && isset($_POST['re_password'])) {
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$uname = validate($_POST['uname']);
+	$pass = validate($_POST['password']);
+
+	$re_pass = validate($_POST['re_password']);
+	$phone = validate($_POST['Phone']);
+    $email = validate($_POST['Email']);
+
+	$user_data = 'uname='. $uname;
+
+
+	if (empty($uname)) {
+		header("Location: signin.php?error=User Name is required&$user_data");
+	    exit();
+	}else if(empty($pass)){
+        header("Location: signin.php?error=Password is required&$user_data");
+	    exit();
+	}
+	else if(empty($re_pass)){
+        header("Location: signin.php?error=Re Password is required&$user_data");
+	    exit();
+	}
+
+	else if(empty($phone)){
+        header("Location: signin.php?error=Phone is required&$user_data");
+	    exit();
+	}
+
+    else if(empty($email)){
+        header("Location: signin.php?error=Email is required&$user_data");
+	    exit();
+	}
+
+	else if($pass !== $re_pass){
+        header("Location: signin.php?error=The confirmation password  does not match&$user_data");
+	    exit();
+	}
+
+	else{
+
+		// hashing the password
+       // $pass = md5($pass);
+
+	    $sql = "SELECT * FROM loginform WHERE User='$uname' ";
+		$result = mysqli_query($con, $sql);
+
+		if (mysqli_num_rows($result) > 0) {
+			header("Location: signin.php?error=The username is taken try another&$user_data");
+	        exit();
+		}else {
+           $sql2 = "INSERT INTO loginform(User, Pass, Email, Phone) VALUES('$uname', '$pass', '$email', '$phone')";
+           $result2 = mysqli_query($con, $sql2);
+           if ($result2) {
+           	 header("Location: signin.php?success=Your account has been created successfully");
+	         exit();
+           }else {
+	           	header("Location: signin.php?error=unknown error occurred&$user_data");
+		        exit();
+           }
+		}
+	}
+	
+}else{
+	header("Location: signin.php");
+	exit();
+}
